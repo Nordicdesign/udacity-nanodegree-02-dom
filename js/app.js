17e1a40185791c7fb73d9659411bb1e38c82a9d4
Alex Lillo <alex.lillo@gmail.com>
2021-05-31 12:30:47 +0100

    nav appears sticky on scroll up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+// ========== NAVIGATION =============================
 const CreateNavigation = () => {
   // get the headers of #recipe-list
   let headers = document.querySelectorAll('#recipe-list h2');
@@ -12,10 +12,8 @@ const CreateNavigation = () => {
   const navElements = document.createDocumentFragment()
 
   for (let i = 0; i < headersText.length; i++) {
-
     const newListItem = document.createElement('li');
     newListItem.innerHTML = "<a href='#section" + i + "'>" + headersText[i] + "</a>";
-
     navElements.appendChild(newListItem);
   }
 
@@ -23,16 +21,15 @@ const CreateNavigation = () => {
   document.getElementById('navigation').appendChild(navElements);
 }
 
-// event listener to smooth navigation links ====================================
+// ========== event listener to smooth navigation links ====================================
 const SmoothLinkOperation = (e) => {
-  console.log("smooooth operatoooooor");
   e.preventDefault();
   const sectionId = e.srcElement.hash
   // get the section coordinates
   const toScrollHeight = document.getElementById(sectionId.slice(1)).offsetTop
   // smoothly scroll to the coordinates
   window.scrollTo({
-    top: toScrollHeight,
+    top: toScrollHeight - 80,
     left: 0,
     behavior: 'smooth'
   });
@@ -41,10 +38,39 @@ const SmoothLinkOperation = (e) => {
 let nav = document.getElementById('navigation')
 nav.addEventListener('click', SmoothLinkOperation)
 
-// event listener to highlight currently viewed section =========================
+// ========== event listener to highlight currently viewed section =========================
 
 
-// back to top ======================================
+// ========== if nav is not visible and user scrolls up ==========================
+// store scroll position globally
+let currentScroll
+let headerVisible
+// is something visible or not?
+let observer = new IntersectionObserver((entries) => {
+  headerVisible = entries[0].isIntersecting
+}, { threshold: [0] });
+
+const NavIsVisible = (e) => {
+  let newScroll = e.target.scrollingElement.scrollTop
+  const nav = document.querySelector('.page__header nav')
+
+  if (currentScroll && newScroll < currentScroll && !headerVisible) {
+    let visible = observer.observe(document.querySelector('.page__header'));
+    currentScroll = newScroll;
+    nav.classList.add('fixed');
+  }
+  else if (headerVisible) {
+    currentScroll = newScroll;
+    nav.classList.remove('fixed');
+  }
+  else {
+    currentScroll = newScroll;
+  }
+}
+
+document.onscroll = NavIsVisible;
+
+// ========== back to top ======================================
 
 
 
