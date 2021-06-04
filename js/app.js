260cd7f16c437af3d045a78316cbff705a5a69f4
Alex Lillo <alex.lillo@gmail.com>
2021-06-04 10:57:30 +0100

    sections are highlighted when on screen

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
-// ========== NAVIGATION =============================
-const CreateNavigation = () => {
+function createNavigation() {
   // get the text meant for the nav of #recipe-list
   let sections = document.querySelectorAll('#recipe-list section');
-  const navLinks = []
+  const navLinks = [];
 
   sections.forEach((section) => {
     navLinks.push({
       title: section.getAttribute("data-nav"),
       link: section.getAttribute("id")
     })
-  })
+  });
 
   // create a list if items
-  const navElements = document.createDocumentFragment()
+  const navElements = document.createDocumentFragment();
 
   for (let link of navLinks) {
     const newListItem = document.createElement('li');
@@ -22,59 +21,99 @@ const CreateNavigation = () => {
 
   // append them to the navigation
   document.getElementById('navigation').appendChild(navElements);
-}
+};
 
-// ========== event listener to smooth navigation links ====================================
-const SmoothLinkOperation = (e) => {
+
+function smoothLinkOperation(e) {
   e.preventDefault();
-  const sectionId = e.srcElement.hash
+  const sectionId = e.srcElement.hash;
   // get the section coordinates
-  const toScrollHeight = document.getElementById(sectionId.slice(1)).offsetTop
+  const toScrollHeight = document.getElementById(sectionId.slice(1)).offsetTop;
   // smoothly scroll to the coordinates
   window.scrollTo({
     top: toScrollHeight - 80,
     left: 0,
     behavior: 'smooth'
   });
+};
+
+
+let currentScroll;
+
+function observeIfVisible(observedTarget, target, className) {
+  // callback
+  let callback = (entries) => {
+    let targetVisible;
+    entries.forEach(entry => entry.isIntersecting ? target.classList.add(className) : target.classList.remove(className) );
+  }
+
+  // init the observer
+  let observer = new IntersectionObserver(callback, { threshold: [0.6] });
+  observer.observe(observedTarget); // start the listener
 }
 
-let nav = document.getElementById('navigation')
-nav.addEventListener('click', SmoothLinkOperation)
-
-// ========== event listener to highlight currently viewed section =========================
 
 
-// ========== if nav is not visible and user scrolls up ==========================
-// store scroll position globally
-let currentScroll
-let headerVisible
-// is something visible or not?
-let observer = new IntersectionObserver((entries) => {
-  headerVisible = entries[0].isIntersecting
-}, { threshold: [0] });
 
-const NavIsVisible = (e) => {
-  let newScroll = e.target.scrollingElement.scrollTop
+
+let headerVisible;
+function isNavVisible(e) {
+  // TODO: investigate scroll up js
+  // gets current scroll position
+  let newScroll = e.target.scrollingElement.scrollTop;
+  // check is header is visible, update headerVisible accordingly
+  let headerCallback = (entries) => {
+    entries.forEach(entry => entry.isIntersecting ? headerVisible = true : headerVisible = false );
+  }
+
+  let observerHeader = new IntersectionObserver(headerCallback, { threshold: [0.3] });
+  let target = document.querySelector('.page__header');
+  observerHeader.observe(target); // start the listener
+
+
+  // add or remove class depending on position and scroll up happening
   const nav = document.querySelector('.page__header nav')
+  // console.log(headerVisible);
 
   if (currentScroll && newScroll < currentScroll && !headerVisible) {
-    let visible = observer.observe(document.querySelector('.page__header'));
     currentScroll = newScroll;
     nav.classList.add('fixed');
   }
-  else if (headerVisible) {
+  else if (headerVisible) { // remove fixed nav if header is visible
     currentScroll = newScroll;
     nav.classList.remove('fixed');
   }
-  else {
+  else { // if user is scrolling down, just update position
     currentScroll = newScroll;
   }
 }
 
 
-// ========== back to top ======================================
+function displayBackToTop(e) {
+  // capture where the user is
+  let newScroll = e.target.scrollingElement.scrollTop;
+  console.log(newScroll);
+  // if below certain Y, display the back to top
+  // if (newScroll > )
+}
 
 
-// execute all the things 
-CreateNavigation()
-document.onscroll = NavIsVisible;
+function observeSections() {
+  const sections = document.querySelectorAll('#recipe-list section')
+
+  for (let section of sections) {
+    observeIfVisible(section, section, 'on-screen')
+  }
+}
+
+function scrollListener(e) {
+  isNavVisible(e);
+  displayBackToTop(e);
+}
+
+// execute all the things
+createNavigation();
+observeSections();
+document.onscroll = scrollListener;
+let nav = document.getElementById('navigation');
+nav.addEventListener('click', smoothLinkOperation);
